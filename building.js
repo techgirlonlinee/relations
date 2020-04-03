15a53791a8671524ee014243c87d6af1ff94288b
Araya Wongwan <hi.arayaa@gmail.com>
2020-04-03 02:21:42 +0200

    changed intro page and description
    added additional rotating animations

--- building.js
+++ building.js
@@ -8,7 +8,6 @@ function getRandomPosition(element) {
 
 window.onload = function() {
     const object = document.createElement("div")
-    object.setAttribute("id", "eachWord");
     object.innerText = "self";
     object.style.color = "#399558";
     document.body.appendChild(object);
@@ -21,45 +20,41 @@ const twoWordArray = ["self", "other"]
 let wordToSelect = 0
 
 const addStamp = function(x, y) {
-
     if (wordToSelect === 0) {
         wordToSelect = 1
     } else {
         wordToSelect = 0
     }
 
-    const word = document.createElement("div") //create an image from scratch
+    const word = document.createElement("div") 
     // word.innerText = "other";
     word.innerText = twoWordArray[wordToSelect];
-    word.style.left = x + "px" // to put it according to the X position that we clicked
-    word.style.top = y + "px" // to put it according to the Y position that we clicked
+    word.style.left = x + "px" 
+    word.style.top = y + "px" 
 
     document.body.appendChild(word)
+    word.setAttribute("id", "eachWord");
 }
 
 document.addEventListener("click", function(event) {
-    addStamp(event.pageX, event.pageY) //print out pixels across the page + pixels down the page, on click
+    addStamp(event.pageX, event.pageY)
 })
 
-const button = document.querySelector('.manifesto')
+const closeButton = document.querySelector('.manifesto')
 const content = document.querySelector('div.manifesto-content')
-button.addEventListener("click", function(event) {
+const viewButton = document.querySelector('a.expand-button')
+closeButton.addEventListener("click", function(event) {
 	console.log("click")
     event.preventDefault()
     content.classList.toggle('open');
+    // content.classList.remove('open');
+    viewButton.classList.toggle('seen');
 })
 
-// 
+viewButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    content.classList.toggle('open');
+    viewButton.classList.toggle('seen');
+})
 
-    // if (content.classList.contains("open")) {
-    //     content.classList.add("open")
-    // } else {
-    //     content.classList.remove("open")
-    // }
-// button.addEventListener("click", function(element, index) {
-//     if (button.classList.contains('.open')) {
-//         button.classList.add(".open")
-//     } else {
-//         button.classList.remove(".open")
-//     }
-// })
+
